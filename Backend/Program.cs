b4880650ca5c31be783593ca742cc19fa23e063c
66Gramms <meszarosricsi01@gmail.com>
2025-01-13 23:35:43 +0100

    Add dev dependencies to backend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,8 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => {
-    return "Hello World!";
-    });
+app.MapGet(
+    "/",
+    () =>
+    {
+        return "Hello World!";
+    }
+);
 
 app.Run();
