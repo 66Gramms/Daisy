83362b5dc152eeddfa5bf420f15be1acec0728bb
66Gramms <meszarosricsi01@gmail.com>
2025-02-07 18:11:36 +0100

    Add user model

---
+++ backend/Models/User.cs
@@ -0,0 +1,17 @@
+namespace backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Role Role { get; set; } = Role.User;
+    }
+
+    public enum Role
+    {
+        SuperAdmin,
+        Admin,
+        User
+    }
+}
